f2bcfe596411311a682ea8dbcee74734c6597588
sultonbayevogabek <sultonbayevogabek@gmail.com>
2025-02-13 18:28:13 +0500

    initial commit

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { ImageComponent } from '../../shared/components/image/image.component';
+import { ImageComponent } from '@shared/components/image/image.component';
 import { RouterLink } from '@angular/router';
-import { CustomButtonComponent } from '../../shared/components/custom-button/custom-button.component';
+import { CustomButtonComponent } from '@shared/components/custom-button/custom-button.component';
 
 @Component({
   selector: 'site-header',
