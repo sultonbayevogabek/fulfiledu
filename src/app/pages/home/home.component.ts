cfb9512c3e992f2305debb5ce14b5e1c967c04e3
sultonbayevogabek <sultonbayevogabek@gmail.com>
2025-03-03 23:10:34 +0500

    commit

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { IntroComponent } from '../../core/intro/intro.component';
 import { HistoryComponent } from './history/history.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @Component({
   selector: 'app-home',
   imports: [
     IntroComponent,
-    HistoryComponent
+    HistoryComponent,
+    ProjectsComponent
   ],
   templateUrl: './home.component.html',
   standalone: true
