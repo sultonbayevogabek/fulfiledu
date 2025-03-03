04a2802718335eaa64442767aea0c520aef0220b
sultonbayevogabek <sultonbayevogabek@gmail.com>
2025-03-03 23:23:19 +0500

    commit

--- src/app/pages/home/projects/projects.component.ts
+++ src/app/pages/home/projects/projects.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { SectionComponent } from '@shared/components/section/section.component';
 import { SectionHeadingComponent } from '@shared/components/section-heading/section-heading.component';
 import { ImageComponent } from '@shared/components/image/image.component';
-import { NgOptimizedImage } from '@angular/common';
+import { NgClass, NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'projects',
@@ -10,7 +10,8 @@ import { NgOptimizedImage } from '@angular/common';
     SectionComponent,
     SectionHeadingComponent,
     ImageComponent,
-    NgOptimizedImage
+    NgOptimizedImage,
+    NgClass
   ],
   templateUrl: './projects.component.html',
   standalone: true
@@ -20,27 +21,34 @@ export class ProjectsComponent {
   projects = [
     {
       title: 'Zoom orqali ta\'lim berish',
-      description: '16-avgust 2020 yildan 2023-yil Sentyabr oyigacha onlayn Zoomda ta\'lim beruvchi IT o\'quv markazi'
+      description: '16-avgust 2020 yildan 2023-yil Sentyabr oyigacha onlayn Zoomda ta\'lim beruvchi IT o\'quv markazi',
+      status: 'finished'
     },
     {
       title: 'Backend dasturlash kursi',
-      description: 'Komiljon Xamidjonov bilan 2023 - sentyabrdan 2024-Sentabr oyichaga backend dasturlash kursi bo’yicha hamkorlik'
+      description: 'Komiljon Xamidjonov bilan 2023 - sentyabrdan 2024-Sentabr oyichaga backend dasturlash kursi bo’yicha hamkorlik',
+      status: 'finished'
     },
     {
       title: 'Russ tili kursi',
-      description: '2023-yil noyabrdan - Hozirga Azizbek Khabibullaev Ayti ekspert bilan frontend dasturlash kursi bo’yicha hamkorlik'
+      description: '2023-yil noyabrdan - Hozirga Azizbek Khabibullaev Ayti ekspert bilan frontend dasturlash kursi bo’yicha hamkorlik',
+      status: 'active'
     },
     {
       title: 'Frontend dasturlash kursi',
-      description: '2023-yil noyabrdan - Hozirga Azizbek Khabibullaev Ayti ekspert bilan frontend dasturlash kursi bo’yicha hamkorlik'
+      description: '2023-yil noyabrdan - Hozirga Azizbek Khabibullaev Ayti ekspert bilan frontend dasturlash kursi bo’yicha hamkorlik',
+      status: 'active'
     },
     {
       title: 'Buxgalteriya kursi',
-      description: '2024-yil may oylaridan - 2024-yil avgust oyiga Bekzod Amanov Gigant Buxgalter kompaniyasi rahbari bilan Buxgalteriya kursi bo’yicha hamkorlik'
+      description: '2024-yil may oylaridan - 2024-yil avgust oyiga Bekzod Amanov Gigant Buxgalter kompaniyasi rahbari bilan Buxgalteriya kursi bo’yicha hamkorlik',
+      status: 'finished'
     },
     {
       title: 'Uzum market kursi',
-      description: '2024-yil Avgust oyidan - 2024-yil Dekabr oygiacha Farhod Ruzmatov Uzumda rasmiy spiker Uzum market savdosi kursi'
+      description: '2024-yil Avgust oyidan - 2024-yil Dekabr oygiacha Farhod Ruzmatov Uzumda rasmiy spiker Uzum market savdosi kursi',
+      status: 'finished'
     }
   ];
+  protected readonly status = status;
 }
