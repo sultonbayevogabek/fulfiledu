cffae4addc03bec8ca19825a6a4e48e2180841e7
sultonbayevogabek <sultonbayevogabek@gmail.com>
2025-03-07 17:20:50 +0500

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MobileMenuComponent } from './core/mobile-menu/mobile-menu.component';
 import { IntroComponent } from './core/intro/intro.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
     MobileMenuComponent,
-    IntroComponent
+    IntroComponent,
+    FooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { ImageComponent } from '@shared/components/image/image.component';
 import { RouterLink } from '@angular/router';
-import { CustomButtonComponent } from '@shared/components/custom-button/custom-button.component';
 import { IconComponent } from '@shared/components/icon/icon.component';
 
 @Component({
@@ -9,14 +8,12 @@ import { IconComponent } from '@shared/components/icon/icon.component';
   imports: [
     ImageComponent,
     RouterLink,
-    CustomButtonComponent,
-    IconComponent,
-    IconComponent,
+    IconComponent
   ],
   templateUrl: './footer.component.html',
   standalone: true,
   host: {
-    class: 'flex items-center justify-between py-2.5 800:py-4 1200:py-5 bg-[#00393B]'
+    class: 'block py-5 bg-[#00393B]'
   },
 })
 
