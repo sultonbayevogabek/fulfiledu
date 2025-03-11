d84643b4dbb5bad2c326937b47e83a7b3d1ff7a0
sultonbayevogabek <sultonbayevogabek@gmail.com>
2025-03-11 19:45:54 +0500

    initial commit

--- src/app/pages/home/contact-us/contact-us.component.ts
+++ src/app/pages/home/contact-us/contact-us.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { SectionComponent } from '@shared/components/section/section.component';
 import { SectionHeadingComponent } from '@shared/components/section-heading/section-heading.component';
 import { NgTemplateOutlet } from '@angular/common';
 import { IconComponent } from '@shared/components/icon/icon.component';
 import { CustomButtonComponent } from '@shared/components/custom-button/custom-button.component';
+import { ContactUsService } from './contact-us.service';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
+import { firstValueFrom, timer } from 'rxjs';
 
 @Component({
   selector: 'contact-us',
@@ -12,11 +16,41 @@ import { CustomButtonComponent } from '@shared/components/custom-button/custom-b
     SectionHeadingComponent,
     NgTemplateOutlet,
     IconComponent,
-    CustomButtonComponent
+    CustomButtonComponent,
+    ReactiveFormsModule,
+    NgxMaskDirective
   ],
-  templateUrl: './contact-us.component.html'
+  templateUrl: './contact-us.component.html',
+  providers: [
+    provideNgxMask(),
+  ]
 })
 
 export class ContactUsComponent {
+  showAlert = signal(false)
+  contactUsForm: FormGroup = new FormGroup({
+    phone: new FormControl('+998 ', [Validators.required]),
+  })
 
+  private contactUsService = inject(ContactUsService);
+
+  async sendNumber() {
+    if (this.contactUsForm.invalid && this.contactUsForm.disabled) {
+      return;
+    }
+
+    this.contactUsForm.disable();
+
+    const formData = new FormData();
+    formData.append('Telefon raqami', '+998 ' + this.contactUsForm.get('phone').value);
+    formData.append('Raqamni qoldirgan vaqti', new Date().toLocaleString());
+
+    await this.contactUsService.sendNumber(formData);
+    this.contactUsForm.enable();
+    this.contactUsForm.get('phone').setValue('+998 ');
+    this.showAlert.set(true);
+
+    await firstValueFrom(timer(5000));
+    this.showAlert.set(false);
+  }
 }
